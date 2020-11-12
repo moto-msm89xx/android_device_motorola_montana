5ff49661989f1477f942d7059cbef3638935ddd1
Jarl-Penguin <jarlpenguin@outlook.com>
2020-11-12 08:22:14 +0300

    montana: Revert back to install-time NFC detection
    Revert "montana: Move NFC permissions to ODM"
    
    This reverts commit 52b2c7fe07f61e3b6b0755ff6d7433cf7013134d.
    
    Revert "montana: Manage NFC with ODM manifests"
    
    This reverts commit 3011165cd353c4e6f50e9d3d88a420c54e9dea59.
    
    Change-Id: I1d040894d9d45ba51ee09f600a19080558b2258c

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -3,8 +3,16 @@
 RADIO=`getprop ro.boot.radio`
 
 rm -rf /system_root/system/priv-app/MotoDoze
-if ["$RADIO" == "Brazil" ] || [ "$RADIO" == "APAC" ] || [ "$RADIO" == "EMEA" ] || [ "$RADIO" == "China" ]; then
-    # Remove e-compass file for APAC, Brazil and EMEA RADIOs
+if [ "$RADIO" != "APAC" ] && [ "$RADIO" != "EMEA" ] && [ "$RADIO" != "China" ]; then
+    # Remove NFC-related files for RADIOs other than APAC, China and EMEA
+    rm /vendor/etc/permissions/com.android.nfc_extras.xml
+    rm /vendor/etc/permissions/android.hardware.nfc.xml
+    rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /vendor/etc/permissions/android.hardware.nfc.hcef.xml
+    rm -r /system_root/system/app/NfcNci
+fi
+if [ "$RADIO" == "Brazil" ] || [ "$RADIO" == "APAC" ] || [ "$RADIO" == "EMEA" ] || [ "$RADIO" == "China" ]; then
+    # Remove e-compass file for APAC, Brazil, China and EMEA RADIOs
     rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
 if [ "$RADIO" == "China" ]; then
