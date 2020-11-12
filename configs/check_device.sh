ba0f9700231537a19ecb6b6797d7d9b335161de3
Jarl-Penguin <jarlpenguin@outlook.com>
2020-11-12 08:28:45 +0300

    montana: Handle mounting in script
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I3aa6e5cd7be739dace51070b9cdf0e488a220c5e

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -2,20 +2,30 @@
 
 RADIO=`getprop ro.boot.radio`
 
-rm -rf /system_root/system/priv-app/MotoDoze
+mkdir /s
+mount /dev/block/bootdevice/by-name/system /s
+mkdir /v
+mount /dev/block/bootdevice/by-name/oem /v
+
+rm -rf /s/system/priv-app/MotoDoze
 if [ "$RADIO" != "APAC" ] && [ "$RADIO" != "EMEA" ] && [ "$RADIO" != "China" ]; then
     # Remove NFC-related files for RADIOs other than APAC, China and EMEA
-    rm /vendor/etc/permissions/com.android.nfc_extras.xml
-    rm /vendor/etc/permissions/android.hardware.nfc.xml
-    rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
-    rm /vendor/etc/permissions/android.hardware.nfc.hcef.xml
-    rm -r /system_root/system/app/NfcNci
+    rm /v/etc/permissions/com.android.nfc_extras.xml
+    rm /v/etc/permissions/android.hardware.nfc.xml
+    rm /v/etc/permissions/android.hardware.nfc.hce.xml
+    rm /v/etc/permissions/android.hardware.nfc.hcef.xml
+    rm -r /s/system/app/NfcNci
 fi
 if [ "$RADIO" == "Brazil" ] || [ "$RADIO" == "APAC" ] || [ "$RADIO" == "EMEA" ] || [ "$RADIO" == "China" ]; then
     # Remove e-compass file for APAC, Brazil, China and EMEA RADIOs
-    rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
+    rm /v/etc/permissions/android.hardware.sensor.compass.xml
 fi
 if [ "$RADIO" == "China" ]; then
-    mv /vendor/etc/libnfc-nxp_retcn.conf /vendor/etc/libnfc-nxp.conf
-    mv /vendor/etc/thermal-engine-retcn.conf /vendor/etc/thermal-engine.conf
+    mv /v/etc/libnfc-nxp_retcn.conf /v/etc/libnfc-nxp.conf
+    mv /v/etc/thermal-engine-retcn.conf /v/etc/thermal-engine.conf
 fi
+
+umount /s
+rmdir /s
+umount /v
+rmdir /v
