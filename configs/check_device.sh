4694bc915d0b1c6e3e6f3324da982a3fc53863f5
Jarl-Penguin <jarlpenguin@outlook.com>
2020-12-14 13:00:57 +0300

    montana: Mount partitions in bash script
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I8c64d378402ca8e6de2bf2529d4976e3a6b099b8

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -2,12 +2,22 @@
 
 RADIO=`getprop ro.boot.radio`
 
-rm -rf /system_root/system/priv-app/MotoDoze
+mkdir -p /s
+mount /dev/block/bootdevice/by-name/system /s
+mkdir -p /v
+mount /dev/block/bootdevice/by-name/oem /s
+
+rm -rf /s/system/priv-app/MotoDoze
 if ["$RADIO" == "Brazil" ] || [ "$RADIO" == "APAC" ] || [ "$RADIO" == "EMEA" ] || [ "$RADIO" == "China" ]; then
     # Remove e-compass file for APAC, Brazil and EMEA RADIOs
-    rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
+    rm /v/etc/permissions/android.hardware.sensor.compass.xml
 fi
 if [ "$RADIO" == "China" ]; then
-    mv /vendor/etc/libnfc-nxp_retcn.conf /vendor/etc/libnfc-nxp.conf
-    mv /vendor/etc/thermal-engine-retcn.conf /vendor/etc/thermal-engine.conf
+    mv /v/etc/libnfc-nxp_retcn.conf /v/etc/libnfc-nxp.conf
+    mv /v/etc/thermal-engine-retcn.conf /v/etc/thermal-engine.conf
 fi
+
+umount /s
+rmdir /s
+umount /v
+rmdir /v
