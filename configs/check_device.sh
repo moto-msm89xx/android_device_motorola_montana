56d873c5a5169b9d9f87a52a72f6c24108bd13be
Jarl-Penguin <jarlpenguin@outlook.com>
2020-12-14 14:32:30 +0300

    montana: Move to SKU-based detection
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I67aa95b9f278e258b21a392de1e4ab0a61607d81

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 
-RADIO=`getprop ro.boot.radio`
+SKU=`getprop ro.boot.hardware.sku`
 
 mkdir -p /s
 mount /dev/block/bootdevice/by-name/system /s
@@ -8,11 +8,10 @@ mkdir -p /v
 mount /dev/block/bootdevice/by-name/oem /s
 
 rm -rf /s/system/priv-app/MotoDoze
-if ["$RADIO" == "Brazil" ] || [ "$RADIO" == "APAC" ] || [ "$RADIO" == "EMEA" ] || [ "$RADIO" == "China" ]; then
-    # Remove e-compass file for APAC, Brazil and EMEA RADIOs
+if [ "$SKU" != "XT1790" ] && [ "$SKU" != "XT1791" ]; then
     rm /v/etc/permissions/android.hardware.sensor.compass.xml
 fi
-if [ "$RADIO" == "China" ]; then
+if [ "$SKU" == "XT1799-2" ]; then
     mv /v/etc/libnfc-nxp_retcn.conf /v/etc/libnfc-nxp.conf
     mv /v/etc/thermal-engine-retcn.conf /v/etc/thermal-engine.conf
 fi
