f1c6c324252641a9d92ed4a2dff6ce222828799c
Jarl-Penguin <jarlpenguin@outlook.com>
2020-12-15 18:41:03 +0300

    montana: Bind mount retcn configs
    * Now we don't have to rely on check_device.sh
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I806287473d2eca1c0753b58a457038f53e2b5f9b

--- configs/check_device.sh
+++
@@ -1,8 +0,0 @@
-#!/sbin/sh
-
-SKU=`getprop ro.boot.hardware.sku`
-
-if [ "$SKU" == "XT1799-2" ]; then
-    mv /vendor/etc/libnfc-nxp_retcn.conf /vendor/etc/libnfc-nxp.conf
-    mv /vendor/etc/thermal-engine-retcn.conf /vendor/etc/thermal-engine.conf
-fi
