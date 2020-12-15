eb7b6034ec73e4d1ab92d09d50cba0249a1217ab
Jarl-Penguin <jarlpenguin@outlook.com>
2020-12-15 08:45:18 +0300

    montana: Don't remove MotoDoze
    * It's not built for montana anymore
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Iefa08ea2b2ca0b5c12cc499fa3693992f3a82577

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -2,7 +2,6 @@
 
 RADIO=`getprop ro.boot.radio`
 
-rm -rf /system_root/system/priv-app/MotoDoze
 if ["$RADIO" == "Brazil" ] || [ "$RADIO" == "APAC" ] || [ "$RADIO" == "EMEA" ] || [ "$RADIO" == "China" ]; then
     # Remove e-compass file for APAC, Brazil and EMEA RADIOs
     rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
